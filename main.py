9ca0e0dc70f683ac1db51478f3f0b0a77e38f143
shurooq <shoroouqalshal@gmail.com>
2026-06-02 20:38:12 +0300

    Requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+@app.get("/welcome")
 def welcome():
     return {
         "message":"Hello World!"
     }
+print([route.path for route in app.routes])
